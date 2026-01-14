d694de00e34d2fae6f9cd73feada018b1711669c
anthon-dev <anthonlouisenoynay123@gmail.com>
2026-01-15 04:07:25 +0800

    feature: note controller

---
+++ backend/src/controllers/noteController.ts
@@ -0,0 +1,9 @@
+import { noteSchema } from "../schemas/noteSchema.js";
+import { Request, Response } from "express";
+import * as service from '../services/noteService.js'
+
+export const addNote = async (req: Request, res: Response) => {
+    const data = noteSchema.parse(req.body)
+    const note = await service.createNote(data.title, data.content)
+    res.status(201).json(note)
+}

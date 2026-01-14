9fbfc4a0d7582c7b4d83494cfdda4dd33c0ea196
anthon-dev <anthonlouisenoynay123@gmail.com>
2026-01-15 04:40:53 +0800

    feature: create note backend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,15 @@
 import express from 'express'
+import noteRoute from './routes/noteRoute.js'
+import { errorHandler } from './middlewares/errorMiddleware.js'
+import { notFound } from './middlewares/notFound.js'
 
 const app = express()
 
 app.use(express.json())
+
+app.use('/notes', noteRoute)
+
+app.use(notFound)
+app.use(errorHandler)
 
 export default app
